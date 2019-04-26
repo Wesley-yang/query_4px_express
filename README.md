ce1439927cb3b6449affbf92ad41b425019b04ee
TrojanAZhen <qq1057987190@hotmail.com>
2019-04-27 00:06:50 +0800

    一个四方转运物流查询并发送短信和邮件的小脚本

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # query_4px_express
+
+> 环境及依赖
+
+```
+# Python3
+
+requests
+
+twilio
+```
+
+> 一个小脚本
+
+**简单的定时查询四方转运快递单号并发送最近的物流信息提醒到手机和邮箱**
+
+> 最近在四方上海淘了些东西，闲着写个脚本
+
+> 邮箱
+
+**用的是QQ邮箱发送邮件，需要去QQ邮箱[申请第三方登录权限](https://mail.qq.com/)**
+
+> 手机短信
+
+**短信是[twilio](https://www.twilio.com/)免费的套餐**
+
+**免费套餐，短信的中文字数限制在11个字**
+
+> 快递查询
+
+**接口用的是[trackingmore](https://my.trackingmore.com/cn.html)提供的，这个逗逼网站识别国内外ip，国内ip申请的api token只能在国内服务器使用，同理：国外申请的api token就只能在国外服务器上用，申请api的时候，注意你的网络环境**
+
+**代码里的查询接口是用的实时模式，20分钟内只允许调用一次，你可以通过申请多个api去测试**
+
+> 关键信息我已经从外部文件导入了
+
+**Secrets.py**
+
+```
+SECRETS = {
+    "twilio": {
+        "sid": "ACxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "token": "ffxxxxxxxxxxxxxxxxxxxxx",
+        "to": "+86xxxxxxxx",
+        "from": "+1xxxxxxxxxxx"
+    },
+
+    "mail": {
+        "from": "xxxxxxx@qq.com",
+        "passwd": "gjxxxxxxxxxxxxxxx",
+        "to": "xxxxxxxxxxxxxxxx@hotmail.com"
+        },
+
+    "trackingmore": {
+        "token": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        }
+    }
+```
+
+> 相关
+
+**代码58行，就是一个字典变字符串而已，不过trackingmore的写法非得加转义字符，不知道是什么鬼鸡儿，懒得改了**
---
+++ query_4px_express.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+import json
+import trackingmoreclass
+
+from smtplib import SMTP_SSL
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+from twilio.rest import Client
+
+from Secrets import SECRETS
+
+SECRETS['']['']
+
+
+tracker = trackingmoreclass.track
+result = ""
+urlStr = ''
+
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
+
+def send_email(express_infos):
+    msg_from = SECRETS['mail']['from']
+    passwd = SECRETS['mail']['passwd']
+    msg_to = SECRETS['mail']['to']
+    subject = '物流信息'
+    words = '<html><h3>物流信息：<h4 style="color: blue">' \
+            + express_infos + '</html></h3></h4>'
+
+    # msg = MIMEText(mail_content, "plain", 'utf-8')
+    msg = MIMEText(words, "html", 'utf-8')
+    msg["Subject"] = Header(subject, 'utf-8')
+    msg["From"] = _format_addr(u'您的物流信息 <%s>' % msg_from)
+    msg["To"] = _format_addr(u'管理员 <%s>' % msg_to)
+
+    try:
+        # ssl登录
+        smtp = SMTP_SSL('smtp.qq.com')
+        # set_debuglevel()用来调试,1开启调试，0关闭调试
+        smtp.set_debuglevel(0)
+        smtp.ehlo('smtp.qq.com')
+        smtp.login(msg_from, passwd)
+        # Send_email
+        smtp.sendmail(msg_from, msg_to, msg.as_string())
+        print("Mail sent successfully!")
+        smtp.quit()
+    except Exception as e:
+        print("Mail delivery failed!")
+
+
+def real_time():
+    urlStr = ""
+    requestData = "{\"tracking_number\": \"DD190411288998\",\"carrier_code\":\"4px\",\"destination_code\":\"CN\",\"tracking_ship_date\": \"\",\"tracking_postal_code\":\"\",\"specialNumberDestination\":\"\",\"order\":\"\",\"order_create_time\":\"\",\"lang\":\"cn\"}"
+    result = tracker.trackingmore(requestData, urlStr, "realtime")
+    decode_str = result.decode()
+    express_infos = ""
+    if json.loads(decode_str)['meta']['code'] == 429:
+        return("API请求过快，请二十分钟后再试!!!")
+    elif json.loads(decode_str)['meta']['code'] == 200 and json.loads(decode_str)['meta']['type'] == 'Success' and json.loads(decode_str)['meta']['message'] == 'Success':
+        json_str = json.loads(decode_str)
+        express_infos = json_str['data']['items'][0]['lastEvent']
+        express_time = json_str['data']['items'][0]['lastUpdateTime']
+        express_email = express_time + " " + express_infos
+    else:
+        express_infos = '单号或查询有误!'
+    return(express_infos, express_email)
+
+
+def main():
+    express_msg, express_email = real_time()
+    express_msg = express_msg[0:8] + "..."
+    send_email(express_email)
+
+    # Your Account SID from twilio.com/console
+    account_sid = SECRETS['twilio']['sid']
+    # Your Auth Token from twilio.com/console
+    auth_token  = SECRETS['twilio']['token']
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+            to=SECRETS['twilio']['to'], 
+            from_=SECRETS['twilio']['from'],
+            body=express_msg
+    )
+
+    print(message.sid)
+
+if __name__ == '__main__':
+    main()
+
